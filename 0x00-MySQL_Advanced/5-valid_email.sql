0261c7f484c983f3f8e3c49e482f7b6fe95e81f1
Francis Thore <thorefrancis@gmail.com>
2024-08-22 22:35:11 +0200

    BEFORE TROGGER

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,12 @@
+-- Creates a trigger that resets the valid_email 
+-- only after the email field has been changed
+DELIMITER //
+CREATE TRIGGER check_update
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+  IF OLD.email <> NEW.email THEN
+    SET NEW.valid_email = NOT OLD.valid_email;
+  END IF;
+END //
+DELIMITER ;
