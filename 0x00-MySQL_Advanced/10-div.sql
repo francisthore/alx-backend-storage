41ba0bbd759000fc661bbfdcf1e611dacbf063a3
Francis Thore <thorefrancis@gmail.com>
2024-08-23 00:57:11 +0200

    DECIMAL RETUN

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -2,11 +2,12 @@
 -- Safely and returns 0 if the quotient is 0
 DELIMITER //
 CREATE FUNCTION SafeDiv(a INT, b INT)
-RETURNS INT
+RETURNS DECIMAL
 BEGIN
   IF b = 0 THEN
     RETURN 0;
+  ELSE
+    RETURN a / b;
   END IF;
-  RETURN a / b;
 END //
 DELIMITER ;
