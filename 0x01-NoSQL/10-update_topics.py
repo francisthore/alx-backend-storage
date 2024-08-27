f09c8279264d2accb450f074c6a08b9ddbc5e227
Francis Thore <thorefrancis@gmail.com>
2024-08-27 21:35:31 +0200

    typing?

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """Defines a function that updates on doc"""
+from typing import List
 
-
-def update_topics(mongo_collection, name, topics):
+def update_topics(mongo_collection, name: str, topics: List[str]):
     """Updates topics of a school doc"""
     mongo_collection.update_many(
         {'name': name},
