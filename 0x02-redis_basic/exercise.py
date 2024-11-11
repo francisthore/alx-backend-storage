17db438d1bd4db63c00d4111aa101e1cceaca08d
Francis Thore <thorefrancis@gmail.com>
2024-11-11 21:58:10 +0200

    call on stor

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -44,6 +44,7 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """ Stores data into redis using random key """
         key = str(uuid.uuid4())
