3a1c46b7bbeceb40e72b8ff8f587ef434d9422c6
Francis Thore <thorefrancis@gmail.com>
2024-11-11 16:03:34 +0200

    average

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python 3
+"""
+    Module that defines a function that calculate avg
+    score for students and sort them
+"""
+
+
+def top_students(mongo_collection):
+    """
+    Returns all students sorted by their average score in descending order.
+    Each document in the result includes an 'averageScore' field.
+    """
+    pipeline = [
+        {
+            "$addFields": {
+                "averageScore": {"$avg": "$topics.score"}
+            }
+        },
+        {
+            "$sort": {"averageScore": -1}
+        }
+    ]
+
+    return list(mongo_collection.aggregate(pipeline))
